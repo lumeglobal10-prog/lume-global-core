0c6a686d98f7e6034da0580bd0c159e8e3b87136
lumeglobal10-prog <lumeglobal10@gmail.com>
2026-03-10 12:28:04 -0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { unoptimized: true },
+  output: 'standalone',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+};
+
+export default nextConfig;
