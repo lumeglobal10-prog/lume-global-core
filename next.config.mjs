e20ea13d6c574227884361e9a859e6c7633c6043
lumeglobal10-prog <lumeglobal10@gmail.com>
2026-03-11 12:26:20 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { unoptimized: true },
-  output: 'standalone',
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
-};
-
-export default nextConfig;
