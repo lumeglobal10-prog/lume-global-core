a253754ed160a8908163867a3540afcd0f087fed
lumeglobal10-prog <lumeglobal10@gmail.com>
2026-03-13 08:37:19 -0300

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  const country = request.geo?.country || 'US';
-  const response = NextResponse.next();
-  response.headers.set('x-lume-geo', country);
-  return response;
-}
