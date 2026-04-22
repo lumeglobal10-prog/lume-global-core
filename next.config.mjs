a2444d37a90638fc07572e655166e3d5139611c6
lumeglobal10-prog <lumeglobal10@gmail.com>
2026-04-22 15:27:04 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 🚀 ACTIVACIÓN DE SALIDA ESTÁTICA MANDATORIA
   output: 'export',
-  images: { unoptimized: true },
+
+  // 🛡️ RESTRICCIÓN DE OPTIMIZACIÓN: DELEGADA AL KERNEL AION
+  images: {
+    unoptimized: true,
+  },
+
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+
+  // 🌐 RE-DIRECCIONAMIENTO DE CANALES (NODO SAN PABLO)
   env: {
     NEXT_PUBLIC_API_URL: '/api/v1',
     NEXT_PUBLIC_RENDER_URL: '/api/v1/render',
